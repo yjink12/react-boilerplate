37c73b67f2b7babaeb585ecb1d73c06494fe0ea7
yjink12 <yjin.k12@gmail.com>
2025-06-11 13:19:50 +0900

    [Feat] meta, og tag, semantic tag 적용 - [x] 기능 추가 - [ ] 기능 수정 - [ ] 기능 삭제 - [ ] 버그 수정 - [ ] 리팩토링, 코드 포맷팅 - [ ] CI/CD, 환경변수 - [ ] 문서 작업 resolves

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Router from './router';
 import ModalComponent from './components/modal/ModalComponent';
 import { cn } from './utils/cn';
 import { useDeviceMediaQuery } from './hook/mediaQuery';
+import { Separator } from './components/ui';
 
 function App() {
   const currentPath = useLocation();
@@ -12,16 +13,26 @@ function App() {
 
   return (
     <div className="App">
-      <div
+      <header>
+        <Header pathName={currentPath.pathname} />
+      </header>
+      <article
         className={cn([
-          'w-full h-auto mx-auto bg-white',
+          'w-full min-h-[80vh] mx-auto bg-white flex-1',
           isMobile ? 'w-[360px]' : 'w-[768px]',
         ])}
       >
-        <Header pathName={currentPath.pathname} />
-        <Router />
-        <ModalComponent />
-      </div>
+        <section>
+          <Router />
+        </section>
+        <section>
+          <ModalComponent />
+        </section>
+      </article>
+      <footer className="mt-auto p-4 pb-5 text-gray-500">
+        <Separator className="my-8" />
+        Github | @yjink12
+      </footer>
     </div>
   );
 }
